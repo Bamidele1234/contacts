a153921a6aba1670e4c4d5c7fa7be29008d456fe
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-10-18 15:37:16 +0100

    Working on the Contacts page

--- lib/pages/main_screen.dart
+++ lib/pages/main_screen.dart
@@ -5,6 +5,46 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Column(
+            children: [
+              const Text(
+                "Contacts",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFFE9EBEC),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: const [
+                  Text('Recently Added'),
+                  SizedBox(
+                    height: 50,
+                    width: double.infinity,
+                  ),
+                  Text('Contacts (125)'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
